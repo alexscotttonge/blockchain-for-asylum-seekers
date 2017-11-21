3afc1e1f6ca6e49a9ca3ac16f7bc311749f8479f
Pablo <paul.ladiv123@gmail.com>
2017-11-21 15:44:00 +0000

    add signup/login/logout button on the index page

--- test/features/test-routes.js
+++ test/features/test-routes.js
@@ -3,18 +3,26 @@ var Browser = require('zombie');
 var assert = require('assert');
 
 
-    describe('Home page', function () {
+describe('Home page', function() {
 
-        before(function (done) {
-            this.browser = new Browser({site: 'http://localhost:3000'});
-            this.browser.visit('/', done);
-        });
-
-        it('should display the index page', function () {
-            assert.ok(this.browser.success);
-        });
-
-        // it('has a button to Sign up here !', function () {
-        //     assert(this.browser.text('a'), 'Sign up here !');
-        // });
+  before(function(done) {
+    this.browser = new Browser({
+      site: 'http://localhost:3000'
     });
+    this.browser.visit('/', done);
+  });
+
+  it('should display the index page', function() {
+    assert.ok(this.browser.success);
+  });
+
+  it('has a button to Sign up here !', function() {
+    assert(this.browser.text('a'), 'Sign up here !');
+    assert(this.browser.text('a'), 'login');
+    assert(this.browser.text('a'), 'logout');
+  });
+
+  // it('has a button to Sign up here !', function () {
+  //     assert(this.browser.text('a'), 'Sign up here !');
+  // });
+});
