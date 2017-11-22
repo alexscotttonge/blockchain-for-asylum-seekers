847c562ed445b958b1af51ebbbf2d0bdee69bc93
ainsleybc <ab.chang@me.com>
2017-11-22 22:20:44 +0000

    separated users  controller from routes

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,31 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var User = require("../models/user");
-var passport = require("passport");
-var LocalStrategy = require("passport-local");
-var  passportLocalMongoose = require("passport-local-mongoose");
+var usersController = require('../controllers/users');
 
-router.get('/new', function(req, res) {
-  res.render('users/new', {
-    title: 'Sign Up form '
-  });
-});
+router.get('/new', usersController.new);
 
-router.post('/', function(req, res) {
-  req.body.username
-  req.body.password
-  User.register(new User({
-    username: req.body.username
-  }), req.body.password, function(err, user) {
-    if (err) {
-      console.log(err);
-      return res.render('users/new');
-    }
-    passport.authenticate("local")(req, res, function() {
-      res.redirect("/applicants");
-    });
-  });
-
-});
+router.post('/', usersController.create);
 
 module.exports = router;
