0d1238f34b168ea1c1f74ded073e98ceb5897006
ainsleybc <ab.chang@me.com>
2017-11-24 13:50:15 +0000

    more refactoring of ethAccounts

--- app/controllers/ethAccounts.js
+++ app/controllers/ethAccounts.js
@@ -10,7 +10,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 module.exports = {
   
-  create: function (req, res, next) {
+  create: (req, res, next) => {
     var newEthAccount, contract, account;
     Application.findById(req.body.application, async function (err, app) {
       account = await createAccount();
@@ -36,24 +36,30 @@ async function createAccount() {
 }
 
 async function deployContract(owner, application) {
-  var account, contract, contractAddress, instance, input, output, bytecode, abi;
+  var account, contractAddress;
+  await web3.eth.getAccounts().then((accounts) => { account = accounts[0]; })
+  await getContract()
+  .deploy({
+    data: '0x' + bytecode,
+    arguments: [owner, application.applicantName, application.applicantDob]
+  })
+  .send({
+    from: account,
+    gas: 3000000
+  })
+  .on('receipt', (receipt) => {
+    contractAddress = receipt.contractAddress
+  })
+  return contractAddress;
+}
+
+function getContract() {
+  var input, output, abi;
   input = fs.readFileSync('./contracts/Identity.sol');
   output = solc.compile(input.toString(), 1);
   bytecode = output.contracts[':Identity'].bytecode;
   abi = JSON.parse(output.contracts[':Identity'].interface);
-  contract = new web3.eth.Contract(abi);
-  await web3.eth.getAccounts().then((accounts) => { account = accounts[0]; })
-  await contract
-    .deploy({
-      data: '0x' + bytecode,
-      arguments: [owner, application.applicantName, application.applicantDob]
-    })
-    .send({
-      from: account,
-      gas: 3000000
-    })
-    .on('receipt', function (receipt) {
-      contractAddress = receipt.contractAddress
-    })
-  return contractAddress;
+  return new web3.eth.Contract(abi);
 }
+
+var bytecode;
