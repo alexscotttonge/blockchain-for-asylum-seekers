9ecdfd702b3ff0153cca78863d7231c4ff199107
ainsleybc <ab.chang@me.com>
2017-11-24 11:07:01 +0000

    refactored ethAccounts controller

--- app/controllers/ethAccounts.js
+++ app/controllers/ethAccounts.js
@@ -1,58 +1,48 @@
 'use strict';
 
-var mongoose = require('mongoose')
-var Application = mongoose.model('application');
-var EthAccount = mongoose.model('ethAccount');
-var Web3 = require('web3');
-var fs = require('fs');
+const mongoose = require('mongoose')
+const Application = mongoose.model('application');
+const EthAccount = mongoose.model('ethAccount');
+const fs = require('fs');
 const solc = require('solc');
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 module.exports = {
-
+  
   create: function (req, res, next) {
-    var application;
-    var contract;
-    var account;
+    var newEthAccount, contract, account;
     Application.findById(req.body.application, async function (err, app) {
-      application = app
       account = await createAccount();
-      contract = await deployContract(account, application);  
-      var newEthAccount = new EthAccount({
-        applicationId: application,
+      contract = await deployContract(account, app);  
+      newEthAccount = new EthAccount({
+        applicationId: app,
         accountAddress: account,
         contractAddress: contract
       });
-      newEthAccount.save()
-        .then(item => {
-          res.redirect('/applications/' + application._id);
-        })
-        .catch(err => {
-          res.render('applications');
-        })
+      try {
+        await newEthAccount.save()
+        es.redirect('/applications/' + app._id);
+      } catch (err) {
+        res.render('applications');
+      }
     })
   }
 }
 
-function createAccount() {
-  var web3 = new Web3();
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  var newAccount = web3.eth.accounts.create();
+async function createAccount() {
+  var newAccount = await web3.eth.accounts.create();
   return newAccount.address;
 }
 
 async function deployContract(owner, application) {
-  var web3 = new Web3();
-  var contract;
-  var contractAddress;
-  var instance;
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  const input = fs.readFileSync('./contracts/Identity.sol');
-  const output = solc.compile(input.toString(), 1);
-  const bytecode = output.contracts[':Identity'].bytecode;
-  const abi = JSON.parse(output.contracts[':Identity'].interface);
-  var account;
-  await web3.eth.getAccounts().then((accounts) => { account = accounts[0]; })
+  var account, contract, contractAddress, instance, input, output, bytecode, abi;
+  input = fs.readFileSync('./contracts/Identity.sol');
+  output = solc.compile(input.toString(), 1);
+  bytecode = output.contracts[':Identity'].bytecode;
+  abi = JSON.parse(output.contracts[':Identity'].interface);
   contract = new web3.eth.Contract(abi);
+  await web3.eth.getAccounts().then((accounts) => { account = accounts[0]; })
   await contract
     .deploy({
       data: '0x' + bytecode,
